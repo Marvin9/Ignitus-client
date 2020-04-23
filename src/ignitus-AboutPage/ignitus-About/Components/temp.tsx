2d1f099ea0780aed13a15ddd75a02926569256d3
Marvin9 <marvinduff97@gmail.com>
2020-04-24 00:31:05 +0530

    Temp

--- src/ignitus-AboutPage/ignitus-About/Components/temp.tsx
+++ src/ignitus-AboutPage/ignitus-About/Components/temp.tsx
@@ -3,7 +3,7 @@ import { IgnitusIntroduction, AboutImage } from '../constants';
 import { withErrorBoundary } from '../../../ignitus-Shared';
 
 import {CoreTeam} from '../../ignitus-CoreTeam/Components';
-import {default as Team } from "../../ignitus-Team";
+import {default as Team} from "../../ignitus-Team";
 import * as S from '../styles'
 
 const About: React.FunctionComponent = withErrorBoundary(() => (
